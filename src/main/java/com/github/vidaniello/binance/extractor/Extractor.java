c477358dfe6171d6fc0dd1ae508d68955feedf30
vidaniello@gmail.com <vidaniello@gmail.com>
2020-10-28 00:10:21 +0100

    first extraction, candlestick to csv

--- src/main/java/com/github/vidaniello/binance/extractor/Extractor.java
+++ src/main/java/com/github/vidaniello/binance/extractor/Extractor.java
@@ -1,23 +1,68 @@
 package com.github.vidaniello.binance.extractor;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
-import com.binance.api.client.domain.account.TradeHistoryItem;
-import com.binance.api.client.impl.BinanceApiService;
+import com.binance.api.client.domain.market.Candlestick;
+import com.binance.api.client.domain.market.CandlestickInterval;
 
 public class Extractor {
 	
 	
-	public void extractOldTrade() {
-		try {
+	//List<TradeHistoryItem> trades = client.getHistoricalTrades("INJBUSD", null, 0l);
+	
+	private BinanceApiRestClient client;
+	private BinanceApiRestClient getRestClient() {
+		if(client==null) {
 			String apiKey = System.getProperty("apikey");
 			String secret = System.getProperty("secret");
-			BinanceApiRestClient client = BinanceApiClientFactory.newInstance(apiKey,secret).newRestClient();
-			List<TradeHistoryItem> trades = client.getHistoricalTrades("INJBUSD", null, 0l);
+			client = BinanceApiClientFactory.newInstance(apiKey,secret).newRestClient();
+		}
+		return client;
+	}
+	
+	public static final String[] candleStickHeader = new String[] {
+			"openTime", "open", "high", "low", "close", "closeTime", "volume", 
+			"quoteAssetVolume", "numberOfTrades", "takerBuyBaseAssetVolume", "takerBuyQuoteAssetVolume"};
+	/**
+	 * Last 500 candleStick to outputStream in csvFormat
+	 * @param os
+	 * @param symbol
+	 * @param interval
+	 */
+	public void extractKlines(Appendable outputStream, CSVFormat csvFormat, String symbol, CandlestickInterval interval) {
+		try {
 			
 			
+			List<Candlestick> candles = getRestClient().getCandlestickBars(symbol, interval);
+			
+			CSVPrinter csvPrinter = new CSVPrinter(outputStream, csvFormat.withHeader(candleStickHeader));
+			
+			candles.forEach(candle->{
+				try {
+					csvPrinter.printRecord(
+							candle.getOpenTime().toString(),
+							candle.getOpen(),
+							candle.getHigh(),
+							candle.getLow(),
+							candle.getClose(),
+							candle.getCloseTime().toString(),
+							candle.getVolume(),
+							candle.getQuoteAssetVolume(),
+							candle.getNumberOfTrades().toString(),
+							candle.getTakerBuyBaseAssetVolume(),
+							candle.getTakerBuyQuoteAssetVolume()
+							);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
 			
 			int i = 0;
 		} catch (Exception e) {
--- src/test/java/com/github/vidaniello/binance/extractor/TestOne.java
+++ src/test/java/com/github/vidaniello/binance/extractor/TestOne.java
@@ -1,6 +1,15 @@
 package com.github.vidaniello.binance.extractor;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+
+import org.apache.commons.csv.CSVFormat;
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.binance.api.client.domain.market.CandlestickInterval;
+
+
 
 
 public class TestOne {
@@ -9,8 +18,12 @@ public class TestOne {
 	public void testOne() {
 		
 		try {
-			new Extractor().extractOldTrade();
-			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintWriter pw = new PrintWriter(baos);
+			new Extractor().extractKlines(pw, CSVFormat.DEFAULT.withQuote(null), "INJBUSD", CandlestickInterval.DAILY);
+			pw.close();
+			String csv = baos.toString();
+			Assert.assertTrue(!csv.isEmpty());
 		} catch (Exception e) {
 			throw new AssertionError(e);
 		}
